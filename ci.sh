4ab84309c7c87bdd5598bc6ad54699fb8352b70b
ondraczsk <ondrakraftak@gmail.com>
2017-05-28 12:14:16 +0200

    Update ci.sh

--- ci.sh
+++ ci.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-echo -e "version\nms\nstop\n" | php src/pocketmine/PocketMine.php --no-wizard | grep -v "\[Tesseract] Adding "
+echo -e "version\nms\nstop\n" | php src/pocketmine/PocketMine.php --no-wizard | grep -v "\[IM] Adding "
 if ls plugins/Tesseract/ImagicalMine*.phar >/dev/null 2>&1; then
     echo Server packaged successfully.
 else
